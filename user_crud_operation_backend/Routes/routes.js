3c2e32420aab95c732263f63a5cf5afb1b330f63
manishpatidarsourcefuse <manish.patidar@sourcefuse.com>
2023-09-21 18:19:30 +0530

    Setup postgres and replace json file

--- user_crud_operation_backend/Routes/routes.js
+++ user_crud_operation_backend/Routes/routes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const  { getAllUser, addUser, getUserById,updateUser, deleteUser } = require('../controllers/userController.js')
+const  { getAllUser, addUser, getUserById, updateUser, deleteUser } = require('../controllers/userController.js')
 
 router.get("/user", getAllUser);
 router.post("/user", addUser);
-router.get('/user/:id', getUserById)
-router.put('/user/:id', updateUser)
-router.delete('/user/:id', deleteUser)
+router.get('/user/:id', getUserById);
+router.put('/user/:id', updateUser);
+router.delete('/user/:id', deleteUser);
 
 module.exports = router;
 
