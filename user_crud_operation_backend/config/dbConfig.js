bdede25ec9303f414133f57c45815a5a81adfd50
manishpatidarsourcefuse <manish.patidar@sourcefuse.com>
2023-09-21 18:47:33 +0530

    Revert "Setup postgres and replace json file"
    This reverts commit 3c2e32420aab95c732263f63a5cf5afb1b330f63.

--- user_crud_operation_backend/config/dbConfig.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    user: "postgres",
-    host: "localhost",
-    database: "user",
-    password: "root",
-    port: 5432,
-};
-  
--- user_crud_operation_backend/controllers/userController.js
+++ user_crud_operation_backend/controllers/userController.js
@@ -1,119 +1,74 @@
-const client = require("../database/db.js");
+const fs = require('fs');
+const userPath = './userDetails/userDetails.json';
 
-const addUser = ((req, res) => {
-    const userData = {
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email,
-        phone: req.body.phone,
-        address: req.body.address
-    }
+const getUserData = () => {
+    let userData = fs.readFileSync(userPath)
+    return JSON.parse(userData);
+}
 
-    try {
-        if (!userData) {
-          throw Error("Send data in request body");
+const getAllUser =((req, res) => {
+    fs.readFile(userPath, 'utf8', (err, data) => {
+        if(err){
+            throw err
         }
-        client.query(
-          `INSERT INTO user_detail (first_name, last_name, email, phone, address) VALUES ($1, $2, $3, $4, $5)`,
-          [userData.first_name, userData.last_name, userData.email, userData.phone, userData.address],
-          (err, data) => {
-            res.status(200).json({
-              message: "Created new User",
-            });
-          }
-        );
-      } catch (error) {
-        res.status(500).json({
-          error: error.message,
-          message: "Failed to create new User",
-        });
-      }
+        res.send(JSON.parse(data))
+    })
 })
 
-
-const getAllUser = ((req, res) => {
-  try {
-    client.query("SELECT * FROM user_detail", (err, data) => {
-      if (err) throw err;
-      res.status(200).json({
-        user: data.rows,
-      });
-    });
-  } catch (error) {
-    res.status(500).json({
-      err: error.message,
-      notes: null,
-    });
-  }
+const addUser = ((req,res) => {
+    let body = req.body
+    let newUserId = Math.floor(Math.random() * 100)
+    let id = {
+        "id": newUserId
+    }
+    let existsUserData = getUserData();
+    existsUserData.user.push(Object.assign(body, id))
+    saveUserData(existsUserData)
+    res.send({sucsess: true, message: "User added sucsessfully"})
 })
+
+const saveUserData = (data) => {
+    let userData = JSON.stringify(data);
+    fs.writeFileSync(userPath, userData)
+}
 
 const getUserById = ((req, res) => {
-    try {
-      const { id } = req.params;
-      client.query("SELECT * FROM user_detail WHERE id=$1", [id], (err, data) => {
-        if (err) throw err;
-  
-        res.status(200).json({
-          user: data.rows[0],
-        });
-      });
-    } catch (error) {
-      res.status(500).json({
-        err: err.message,
-        note: null,
-      });
+    let existsUserData =  getUserData()
+    let findUserById = existsUserData.user.find((user) => user.id == req.params.id);
+    if (!findUserById) {
+        res.status(404).send("User with id was not found");
+    } else {
+        res.send(findUserById)
     }
 })
 
 const updateUser = ((req, res) => {
-    const { id } = req.params;
-    const userData = {
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      email: req.body.email,
-      phone: req.body.phone,
-      address: req.body.address
+    let body = req.body
+    let userId = req.params.id
+    let id = {
+        "id": userId
     }
-    
-    try {
-        if (!userData) {
-          throw Error("Send data in request body");
-        }
-        client.query(
-          "UPDATE user_detail SET first_name = $1, last_name = $2, email = $3, phone = $4, address = $5 WHERE id = $6",
-          [userData.first_name, userData.last_name, userData.email, userData.phone, userData.address, id],
-          (err, data) => {
-            if (err) throw err;
-            res.status(200).json({
-              message: "Updated user",
-            });
-          }
-        );
-      } catch (error) {
-        res.status(500).json({
-          err: error.message,
-          message: "Failed to update user",
-        });
-      }
+    let existsUserData = getUserData();
+    let replacement = Object.assign(body, id);
+    let userIndex = existsUserData.user.findIndex((user) => (user.id == replacement.id));
+
+    existsUserData.user[userIndex] = replacement
+    saveUserData(existsUserData)
+
+    res.send({sucsess: true, message: `User with id ${userId} updated successfully`})
 })
 
 
 const deleteUser = ((req, res) => {
-  try {
-    const { id } = req.params;
-    client.query("DELETE FROM user_detail WHERE id=$1", [id], (err, data) => {
-      if (err) throw err;
-      res.status(200).json({
-        error: null,
-        message: "User deleted",
-      });
-    });
-  } catch (error) {
-    res.status(500).json({
-      error: error.message,
-      message: "Failed to delete user",
-    });
-  }
+
+    let userId = req.params.id
+    let existsUserData = getUserData();
+    let userIndex = existsUserData.user.findIndex((user) => (user.id == userId));
+
+    existsUserData.user.splice(userIndex, 1);
+    saveUserData(existsUserData);
+
+    res.send({sucsess: true, message: `User with id ${userId} deleted successfully`})
 })
 
-module.exports = { addUser, getAllUser, getUserById, updateUser, deleteUser}
+module.exports = { getAllUser, addUser, getUserById, updateUser, deleteUser }
