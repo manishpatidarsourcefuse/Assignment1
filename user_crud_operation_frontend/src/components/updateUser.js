a0f6193125ccb4f03c8a46bc9ae32e0ce16baf6f
manishpatidarsourcefuse <manish.patidar@sourcefuse.com>
2023-09-21 18:45:30 +0530

    Revert "update response'"
    This reverts commit ecf629cb2d88a3d8fff8e2baf32129d3da406976.

--- user_crud_operation_frontend/src/components/updateUser.js
+++ user_crud_operation_frontend/src/components/updateUser.js
@@ -39,7 +39,7 @@ const UpdateUser = () => {
     const loadUserData = async (userId) =>{
         let response = await getUserById(userId);
         if(response.status === 200) {
-            setUser(response.data.user);
+            setUser(response.data);
         }else {
             setOpen(true)
             setMessage("User is not exist in list")
